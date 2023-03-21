bfd86df627b5023a64472f4d9f67ab7040280d91
Simon Redman <simon@ergotech.com>
2023-03-20 20:27:45 -0400

    Maybe really add description this time

--- setup.py
+++ setup.py
@@ -10,12 +10,18 @@
 
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='suta_bed',
     version='0.1.3',
     license='MIT',
     author="Simon Redman",
     author_email='simon@ergotech.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages('suta_bed'),
     package_dir={'': 'suta_bed'},
     url='https://github.com/sredman/suta-ble-bed',
